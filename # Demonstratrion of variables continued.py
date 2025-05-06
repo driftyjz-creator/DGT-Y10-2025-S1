3e3055be8778c48b7a6fb29dba7632bbb2c528a8
driftyjz-creator <Jonah.hetrick@Kingsway.school.nz>
2025-05-07 10:22:10 +1200

    Add files via upload
    My first python file

---
+++ # Demonstratrion of variables continued.py
@@ -0,0 +1,20 @@
+# Demonstratrion of variables continued
+#Jonah Hetrick
+#Date: 2/05/2025
+#Version 1.0
+
+#string
+AnimalName = 'Rooster'
+#integer
+AnimalLegs = '2'
+#float
+AnimalHeight = '0.5 metres'
+#boolean
+Animaltail = False
+
+#Display animals details
+print(f"The chosen animal is" {AnimalName}
+print(f"The {AnimalName} has {AnimalLegs} Legs")
+print(f"The {AnimalName}is {AnimalHeight} tall")
+print(f"The {AnimalName} has a tail: {Animaltail}")
+print(Animaltail)
